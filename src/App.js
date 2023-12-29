b3cec070aea093deb996088fb215a8690885681b
Matheus Santos <mateteus07042000@gmail.com>
2023-12-29 10:43:18 -0300

    começo do preparo da estrutura do site, header+

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Main from './Main';
 import LoginForm from './routes/Login.js';
 import RegistrationForm from './routes/Register.js';
 import Central from './routes/Central.js'
-import Error from './routes/Error.jsx'
 import { PrivateRoute } from './auth.js';
 import { CookiesProvider } from 'react-cookie';
+import Error from './routes/Error.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main />,
+    element: <LoginForm />,
     errorElement: <Error/>,
-    children: [
-      {
-        path: "/",
-        element: <LoginForm />,
-      },
-      {
-        path: "/register",
-        element: <RegistrationForm />,
-      },
-      {
-        path: "/central",
-        element:
-        <CookiesProvider>
-          <PrivateRoute>
-            <Central />
-          </PrivateRoute> 
-        </CookiesProvider>
-      },
-    ]
+  },
+  {
+    path: "/register",
+    element: <RegistrationForm />,
+  },
+  {
+    path: "/central",
+    element:
+    <CookiesProvider>
+      <PrivateRoute>
+        <Central />
+      </PrivateRoute> 
+    </CookiesProvider>
   },
 ]);
 
 function App() {
-
   return (
     <React.StrictMode>
       <RouterProvider router={router}/>
